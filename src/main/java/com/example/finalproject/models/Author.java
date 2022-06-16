c15b1416d756cc956dbe65df68482c13a8914b5d
Danil Zinoveev <dzinoveev@gmail.com>
2022-06-16 23:42:57 +0300

    added model "Author" and POST

---
+++ src/main/java/com/example/finalproject/models/Author.java
@@ -0,0 +1,63 @@
+package com.example.finalproject.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private Byte rate;
+
+    @Column
+    private String nickname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getRate() {
+        return rate;
+    }
+
+    public void setRate(Byte rate) {
+        this.rate = rate;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
