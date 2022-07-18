7a3f1e897da6c7e6c311bc5bba7570e04708415c
SimacheV <vodolas@meta.ua>
2022-07-18 16:05:24 +0300

    Исправил

--- src/main/java/com/example/finalproject/keys/AuthorSubscriberKey.java
+++ src/main/java/com/example/finalproject/keys/AuthorSubscriberKey.java
@@ -6,19 +6,4 @@ public class AuthorSubscriberKey implements Serializable {
     private Long authorId;
     private Long subscriberId;
 
-    public Long getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(Long authorId) {
-        this.authorId = authorId;
-    }
-
-    public Long getSubscriberId() {
-        return subscriberId;
-    }
-
-    public void setSubscriberId(Long subscriberId) {
-        this.subscriberId = subscriberId;
-    }
 }
