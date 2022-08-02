80a399ac7c25598a9a8e75a59baf48db93abc166
Danil Zinoveev <dzinoveev@gmail.com>
2022-08-02 23:30:22 +0300

    fixed 8 point

--- src/main/java/com/example/finalproject/repository/AuthorRepository.java
+++ src/main/java/com/example/finalproject/repository/AuthorRepository.java
@@ -12,17 +12,4 @@ import java.util.List;
 @Transactional
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-@Query("SELECT a FROM Author a " +
-        "JOIN AuthorSubscriber ausb on a.id = ausb.authorId " +
-        "JOIN Subscriber s on ausb.subscriberId = s.id " +
-        "WHERE a.rate = ?1 AND s.username = ?2")
-public   List<Author> authorAllParam(byte rate, String subscriber);
-
-public List<Author> getAuthorsByRate(byte rate);
-
-    @Query("SELECT a FROM Author a " +
-            "JOIN AuthorSubscriber ausb on a.id = ausb.authorId " +
-            "JOIN Subscriber s on ausb.subscriberId = s.id " +
-            "WHERE s.username = ?1")
-    public   List<Author> authorSubscriberNameFilter(String subscriber);
 }
